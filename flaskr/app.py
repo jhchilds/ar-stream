078fbc2ecec8cb3307e24dad7917c40511c4f411
joshuachilds <jhchilds@uvm.edu>
2019-09-15 16:29:31 -0400

    debugging socket

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
 from flask import render_template
-# from flask_socketio import SocketIO
+from flask_socketio import SocketIO
+import threading
 
 
 
@@ -9,17 +10,23 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev',
 )
-# socketio = SocketIO(app)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# @socketio.on('message')
-# def handle_message(msg):
-#     print('received message: ' + msg)
+@socketio.on('message')
+def handle_message(msg):
+    print('received message: ' + msg)
 
 # flask-socketio can not use Flask Run command, simply run with python NOT Flask Run.
-# socketio.run(app)
+
+def start_socket():
+    socketio.run(app)
+
+socket_thread = threading.Thread(target=start_socket())
+socket_thread.start()
+
 
