01c94cffec2943a5d4f2cbee87eafaa9fb1132ed
joshuachilds <jhchilds@uvm.edu>
2019-09-15 14:46:48 -0400

    starting web socket for data strea,

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
 
 
 
@@ -8,8 +9,18 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev',
 )
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print('received message: ' + msg)
+
+#flask-socketio can not use Flask Run command, simply run with python NOT Flask Run.
+socketio.run(app)
+
 
