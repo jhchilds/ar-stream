df6fbede0bf6495c7eb339381901e51083351ed0
joshuachilds <jhchilds@uvm.edu>
2019-12-17 15:08:56 -0500

    sending pose data to server from wheel encoder signal

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask import render_template
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/live-position', methods=['GET','POST'])
+def live_position():
+    return render_template('live-pos.html')
+
+@app.route('/stream', methods=['POST'])
+def stream():
+    socketio.emit('data', request.form) #This is for live updating to client with websocket
+    # Below is server side data collection
+    pose_x = request.form["x"]
+    pose_y = request.form["y"]
+    pose_z = request.form["z"]
+
+
+
+    return 'ok'
+
+
+
+
+
+
+socketio.run(app, host="0.0.0.0", port=1142, log_output=True)
+
+
+
+
